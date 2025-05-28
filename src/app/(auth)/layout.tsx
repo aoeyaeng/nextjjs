372bda1980c4d961090c2d01856bbf92ba3c084f
aoeyaeng <aeysirikorn2005@gmail.com>
2025-05-28 12:02:04 +0700

    signup page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { K2D } from "next/font/google";
-import "./globals.css";
-import Navbar01Page from "@/components/navbar-01/navbar-01";
+import "../globals.css";
+
 
 const k2D = K2D({
    subsets: ['thai'],
@@ -10,11 +10,11 @@ const k2D = K2D({
 });
 
 export const metadata: Metadata = {
-  title: "ระบบขายสินค้า COSCI Shop",
+  title: "เข้าสู่ระบบ",
   description: "ขายสินค้าของนักศึกษา ประจำปี 2568",
 };
 
-export default function RootLayout({
+export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -24,7 +24,6 @@ export default function RootLayout({
       <body
         className={`${k2D.className}`}
       >
-        <Navbar01Page />
         
         {children}
       </body>
